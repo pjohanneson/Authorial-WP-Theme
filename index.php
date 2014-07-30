7e80fcdf27d34996f7516c27bb1f83acb804b547
pjohanneson <pjohanneson@Lappy>
2014-07-29 22:19:50 -0500

    Forked Wycks' WP-Skeleton for a base

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * @package WordPress
+ * @subpackage Authorial
+ */
 
-echo( "This is it!" );
-// testing Git
+    get_header();  //the Header
+
+    get_template_part( 'menu', 'index' ); //the  menu + logo/site title
+
+    get_template_part( 'loop', 'index' ); //the Loop
+
+    get_template_part( 'sidebar', 'index' ); //the Sidebar
+
+    get_footer(); //the Footer
+
+?>
+
