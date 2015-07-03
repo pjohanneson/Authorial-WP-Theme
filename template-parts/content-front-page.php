00461f0ca28cb54b0a37b0cf69ae73a308f67f42
Patrick Johanneson <pjohanneson@gmail.com>
2015-07-02 23:23:45 -0500

    Added metadata display to the singular Story template, and associate CSS

--- template-parts/content-front-page.php
+++ template-parts/content-front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template used for displaying page content in page.php
+ * The template used for displaying front page content in front-page.php
  *
  * @package Authorial
  */
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -17,7 +17,47 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+
+		<?php
+		if ( class_exists( 'PJ_Story' ) && is_singular( PJ_Story::POST_TYPE ) ) {
+			// get the publication date
+			$single = true;
+			$pub = get_post_meta( get_the_ID(), PJ_Story::PREFIX . 'publication_group', $single );
+
+			if( $pub && is_array( $pub ) ) {
+				echo '
+				<div class="story-publication-details">
+					Original publication: <a href="' . $pub[ PJ_Story::PREFIX . 'publication_url']
+					.'">' . $pub[ PJ_Story::PREFIX . 'publication'] . '</a>,
+					' . date( get_option( 'date_format' ), $pub[ PJ_Story::PREFIX . 'publication_date'] ) . '
+				</div><!-- .story-publication-details -->' . PHP_EOL;
+			}
+		}	
+		
+		?>
+
 		<?php the_content(); ?>
+		<?php
+		if( class_exists( 'PJ_Story' ) && is_singular( PJ_Story::POST_TYPE ) ) {
+			// get the review content
+			$reviews = get_post_meta( get_the_ID(), PJ_Story::PREFIX . 'review_group' );
+			if( $reviews && is_array( $reviews ) ) {
+				echo( '<div class="story-reviews">' . PHP_EOL );
+				echo( '<h2>Reviews</h2>' . PHP_EOL );
+				foreach( $reviews as $r ) {
+					echo( '<p>' );
+					echo( '<q>' . $r[ PJ_Story::PREFIX . 'review' ] . '</q>' . PHP_EOL );
+					echo( '&mdash;<a href="' . $r[ PJ_Story::PREFIX . 'reviewer_url' ]. '">' .
+						$r[ PJ_Story::PREFIX . 'reviewer' ] .'</a>, ' .
+						date( get_option( 'date_format' ), $r[ PJ_Story::PREFIX . 'review_date'] )
+						);
+					echo( '</p>' . PHP_EOL );
+				}
+
+				echo( '</div><!-- .story-reviews -->' . PHP_EOL );
+			}
+		}
+		?>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'authorial' ),
